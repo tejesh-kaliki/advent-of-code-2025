29d04554cc5dd5ecdddb37db915ed7285e436cb7
Tejesh Kaliki <tejesh.kaliki@gmail.com>
2025-12-07 12:31:58 +0530

    Optimized day 7

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -3,25 +3,22 @@ use std::{fs, time::Instant};
 pub fn run() {
     let (input, t_input) = time!(fs::read_to_string("inputs/day07.txt").unwrap());
     let (parsed, t_parse) = time!(parse_input(&input));
-    let (part1, t_part1) = time!(part1(&parsed));
-    let (part2, t_part2) = time!(part2(&parsed));
+    let ((part1, part2), t_part1_and_2) = time!(solve_both_parts(&parsed));
 
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
 
     println!("Input time: {:?}", t_input);
     println!("Parse time: {:?}", t_parse);
-    println!("Part 1 time: {:?}", t_part1);
-    println!("Part 2 time: {:?}", t_part2);
-    println!("Total time: {:?}", t_part2 + t_part1 + t_parse + t_input);
+    println!("Part 1 & 2 time: {:?}", t_part1_and_2);
+    println!("Total time: {:?}", t_part1_and_2 + t_parse + t_input);
 }
 
-#[derive(Debug)]
 struct Grid {
     width: usize,
     height: usize,
-    cells: Vec<Vec<bool>>,
-    start: (usize, usize),
+    cells: Vec<u8>,
+    start: usize,
 }
 
 fn parse_input(input: &str) -> Grid {
@@ -29,81 +26,57 @@ fn parse_input(input: &str) -> Grid {
         .trim()
         .lines()
         .map(|line| line.as_bytes())
+        .skip_while(|line| !line.contains(&b'S')) // It should be in first row, but just to be sure.
         .collect::<Vec<_>>();
+    let height = lines.len();
+    let width = lines[0].len();
 
-    let mut cells = Vec::with_capacity(lines.len());
-    let mut start = (0, 0);
-    for (y, line) in lines.iter().enumerate() {
-        let mut row = Vec::with_capacity(line.len());
-        for (x, &cell) in line.iter().enumerate() {
-            row.push(cell == b'^');
-            if cell == b'S' {
-                start = (x, y);
-            }
-        }
-        cells.push(row);
+    let mut cells = Vec::with_capacity(width * height);
+    let start = lines[0].iter().position(|&cell| cell == b'S').unwrap();
+    for line in lines {
+        cells.extend(line);
     }
 
     Grid {
-        width: cells[0].len(),
-        height: cells.len(),
+        width,
+        height,
         cells,
         start,
     }
 }
 
-fn part1(grid: &Grid) -> i64 {
-    let mut current_row_state: Vec<bool> = vec![false; grid.width];
-    current_row_state[grid.start.0] = true;
-
-    let mut count = 0;
-    for y in grid.start.1 + 1..grid.height {
-        let row = &grid.cells[y];
-        let mut new_row_state = vec![false; grid.width];
-        for (x, (&state, &cell)) in current_row_state.iter().zip(row).enumerate() {
-            if state && cell {
-                count += 1;
-                if x > 0 {
-                    new_row_state[x - 1] = true;
-                }
-                if x < grid.width - 1 {
-                    new_row_state[x + 1] = true;
-                }
-            } else if state {
-                new_row_state[x] = true;
-            }
-        }
-
-        current_row_state = new_row_state;
-    }
-
-    count
-}
-
-fn part2(grid: &Grid) -> i64 {
+fn solve_both_parts(grid: &Grid) -> (i64, i64) {
     let mut current_row_state: Vec<i64> = vec![0; grid.width];
-    current_row_state[grid.start.0] = 1;
+    let mut next_row_state = vec![0; grid.width];
+    current_row_state[grid.start] = 1;
 
-    for y in grid.start.1 + 1..grid.height {
-        let row = &grid.cells[y];
-        let mut new_row_state = vec![0; grid.width];
-        for (x, (&state, &cell)) in current_row_state.iter().zip(row).enumerate() {
-            if state > 0 && cell {
+    let mut split_count = 0;
+    let w = grid.width;
+    for y in 1..grid.height {
+        next_row_state.fill(0);
+        for x in 0..w {
+            let state = current_row_state[x];
+            if state == 0 {
+                continue;
+            }
+
+            if grid.cells[y * w + x] == b'^' {
+                split_count += 1;
                 if x > 0 {
-                    new_row_state[x - 1] += state;
+                    next_row_state[x - 1] += state;
                 }
-                if x < grid.width - 1 {
-                    new_row_state[x + 1] += state;
+                if x + 1 < w {
+                    next_row_state[x + 1] += state;
                 }
-            } else if state > 0 {
-                new_row_state[x] += state;
+            } else {
+                next_row_state[x] += state;
             }
         }
 
-        current_row_state = new_row_state;
+        std::mem::swap(&mut current_row_state, &mut next_row_state);
     }
 
-    current_row_state.iter().sum()
+    (split_count, current_row_state.iter().sum())
 }
 
 #[cfg(test)]
@@ -130,7 +103,6 @@ mod tests {
 ..............."
             .trim();
         let grid = parse_input(input);
-        assert_eq!(part1(&grid), 21);
-        assert_eq!(part2(&grid), 40);
+        assert_eq!(solve_both_parts(&grid), (21, 40));
     }
 }
